d055db33bb87d6857ef60207b5224605c8dff79e
stropee <stropee@campus-eni.fr>
2026-01-14 16:55:20 +0100

    TP 6

--- src/app/components/carbon-footprint-form/carbon-footprint-form.ts
+++ src/app/components/carbon-footprint-form/carbon-footprint-form.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CarbonFootprintCompute} from '../../services/carbon-footprint-compute';
 
 @Component({
   selector: 'app-carbon-footprint-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './carbon-footprint-form.html',
   styleUrl: './carbon-footprint-form.css',
   standalone: true
 })
 export class CarbonFootprintForm {
+
+
+  public travelForm: FormGroup
+
+  constructor(private cfc: CarbonFootprintCompute) {
+    this.travelForm = new FormGroup(
+      {
+        distance: new FormControl(null, [Validators.min(1), Validators.required]),
+        consumption: new FormControl(null, [Validators.min(1), Validators.required]),
+        date: new FormControl(null, Validators.required),
+        travelType : new FormControl(null, [Validators.pattern(/(car|train|plane)/)])
+      }
+    )
+  }
+
+  onTravelSubmit() {
+    if (this.travelForm.valid) {
+      let value = this.travelForm.value
+
+      let quantityCo2 = this.cfc.getQuantityCo2ByTravel(value.distance, value.consumption);
+      this.cfc.addTravel(
+        {distance: value.distance, consumptionPer100: value.consumption, quantityCo2: quantityCo2}
+      )
+    }
+
+  }
 
 }
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,22 +1,42 @@
 import { Component } from '@angular/core';
 import {User} from '../../services/user';
 import {Router} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
   standalone: true
 })
 export class Home {
 
+  userLogin: string = "";
+  password: string = "";
+  errors : string[] = [];
+
   constructor(private userService : User, private router : Router) {
   }
 
   login(){
-    this.userService.login("Michel")
-    this.goToSummary()
+
+    this.errors = [];
+
+    if(this.userLogin?.trim().length < 3){
+      this.errors.push("L'identifiant doit avoir au moins 3 caractères")
+    }
+
+    if(this.password?.trim().length < 6){
+      this.errors.push("Le mot doit avoir au moins 6 caractères")
+    }
+
+    if(this.errors.length == 0){
+      this.userService.login(this.userLogin)
+      this.goToSummary()
+    }
   }
 
   goToSummary(){
